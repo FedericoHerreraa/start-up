f2d22ac4ed08aee451ef450c47d13eda06400223
FedericoHerreraa <fede.juan.herrera@gmail.com>
2024-12-09 12:06:17 -0300

    adding homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+import { OurWay } from "./components/OurWay";
+import { Header } from "./components/Header"
+import { LandingInfo } from "./components/Landing"
+import { OurWork } from "./components/OurWork";
+import { CustomSeparator } from "./components/CustomSeparator";
+import { Founders } from "./components/Founders";
+import { StartNow } from "./components/StartNow";
+import { Footer } from "./components/Footer";
+import { LineSeparator } from './components/LineSeparator';
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Hola</h1>
-    </div>
-  );
+
+export default function HomePage() {
+    return (
+        <>
+            <Header />
+            <LandingInfo />
+            <CustomSeparator />
+            <OurWay />
+            <LineSeparator />
+            <OurWork />
+            <CustomSeparator />
+            <Founders /> 
+            <LineSeparator />
+            <StartNow />
+            <Footer />
+        </>
+    )
 }
