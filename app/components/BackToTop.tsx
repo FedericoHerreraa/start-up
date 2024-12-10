73aee8bf4614395f6f643a74d110c0d37bb7a3e2
FedericoHerreraa <fede.juan.herrera@gmail.com>
2024-12-10 09:41:59 -0300

    .

--- app/components/BackToTop.tsx
+++ app/components/BackToTop.tsx
@@ -1,11 +1,15 @@
 'use client'
+
 import { IoArrowUp } from "react-icons/io5";
+import { useNightMode } from "../context/NightModeContext";
 
 export const BackToTop = () => {
+    const { nightMode } = useNightMode();
+
     return (
         <div 
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-            className="border border-zinc-600 rounded-full fixed bottom-4 right-4 p-2 bg-zinc-300 cursor-pointer shadow-lg hover:scale-110 transition-transform duration-300 z-50"
+            className={`border ${nightMode ? 'bg-zinc-300 border-zinc-600' : 'border-zinc-300 bg-white'} rounded-full fixed bottom-4 right-4 p-2  cursor-pointer shadow-lg hover:scale-110 transition-transform duration-300 z-50`}
         >
             <IoArrowUp size={20}/>
         </div>
