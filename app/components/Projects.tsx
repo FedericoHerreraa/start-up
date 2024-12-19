7e6c23c8a47c2d56892d585f4427e52d7353c312
Federico Herrera <fede.juan.herrera@gmail.com>
2024-12-19 11:13:12 -0300

    adding pictures to projects and improving description

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,6 +3,10 @@
 
 import { useLenguage } from "@/app/context/LenguageContext"
 import { useNightMode } from "@/app/context/NightModeContext"
+import pinascoImg from '@/app/img/pinasco-project.png'
+import { ArrowRight } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
 
 export const Projects = () => {
     const { nightMode } = useNightMode()
@@ -20,9 +24,27 @@ export const Projects = () => {
                                 : 'from-red-700 to-red-800'
                         } bg-clip-text text-transparent`}>{spanish ? item.titleSpanish : item.titleEnglish} </p>
                     </div>
-                    <div className="flex md:flex-row flex-col gap-5 w-[95%] mx-auto">
-                        <div className={`md:w-1/2 ${nightMode ? 'bg-zinc-900' : 'bg-zinc-100'} rounded-2xl md:h-96 h-60`}></div>
-                        <div className={`md:w-1/2 ${nightMode ? 'bg-zinc-900' : 'bg-zinc-100'} rounded-2xl md:h-96 h-60`}></div>
+                    <div className={`flex ${index%2 === 0 ? 'md:flex-row flex-col' : 'md:flex-row-reverse flex-col' }  gap-5 w-[95%] mx-auto`}>
+                        <div className={`md:w-1/2 md:h-96 h-60 ${index%2 === 0 ? 'mr-40' : 'ml-40 text-right'}`}>
+                            <p className="mt-10 text-zinc-400">{spanish ? item.descriptionSpanish : item.descriptionEnglish}</p>
+                            <h2 className="mt-10 text-lg font-semibold">{spanish ? 'Nuestro desafio:' : 'Our Challenge:'}</h2>
+                            <p className="mt-5">{spanish ? item.desafioSpanish : item.desafioEnglish}</p>
+                            <div className={`flex ${index%2 === 0 ? 'justify-start' : 'justify-end'} `}>
+                                <Link href={`https://pinasco-three.vercel.app`} className="flex items-center gap-2 border border-blue-400 bg-zinc-900 hover:bg-zinc-800 px-7 py-2 rounded-lg mt-14 w-fit">
+                                    <p>Visitar</p>
+                                    <ArrowRight size={20}/>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="border-x-2 border-t-2 border-zinc-600 rounded-t-[26px]">
+                            <Image
+                                src={item.image}
+                                alt="foto de pinasco"
+                                width={1000}
+                                height={600}
+                                className="rounded-t-3xl"
+                            />
+                        </div>
                     </div>
                 </section> 
             ))}
@@ -33,23 +55,32 @@ export const Projects = () => {
 const info = [
     {
         id: 1,
-        titleSpanish: 'Consultorio Odontologíco.',
+        titleSpanish: 'Consultorio Odontológico.',
         titleEnglish: 'Dental Clinic Website.',
-        description: 'Diseño de una página web para una clínica odontológica. El proyecto incluye un formulario de contacto, una galería de imágenes y una sección de servicios.',
-        image: ''
+        descriptionSpanish: 'Diseño de una página web para una clínica odontológica. El proyecto incluye un formulario de contacto, una galería de imágenes y una sección de servicios.',
+        descriptionEnglish: 'Design of a website for a dental clinic. The project includes a contact form, an image gallery, and a services section.',
+        desafioSpanish: 'El desafío principal fue lograr que el diseño transmita confianza y profesionalismo, asegurando que los pacientes se sientan cómodos al navegar por el sitio. Además, se trabajó en destacar los servicios clave de la clínica y proporcionar una experiencia amigable en dispositivos móviles.',
+        desafioEnglish: 'The main challenge was to ensure that the design conveys trust and professionalism, while making patients feel comfortable browsing the site. Additionally, the focus was on highlighting the clinic\'s key services and providing a mobile-friendly experience.',
+        image: pinascoImg
     },
     {
         id: 2,
         titleSpanish: 'Tienda de Pizzas.',
         titleEnglish: 'Pizzeria Web Shop.',
-        description: 'Desarrollo de una aplicación web para una pizzería. El proyecto incluye un carrito de compras, un sistema de pagos y una sección de promociones.',
-        image: ''
+        descriptionSpanish: 'Desarrollo de una aplicación web para una pizzería. El proyecto incluye un carrito de compras, un sistema de pagos y una sección de promociones.',
+        descriptionEnglish: 'Development of a web application for a pizzeria. The project includes a shopping cart, a payment system, and a promotions section.',
+        desafioSpanish: 'El desafío fue integrar un sistema de pedidos intuitivo que permita a los usuarios personalizar sus pizzas de manera sencilla. También se trabajó en garantizar tiempos de respuesta rápidos y una interfaz atractiva para promover las promociones.',
+        desafioEnglish: 'The challenge was to integrate an intuitive ordering system that allows users to customize their pizzas easily. Additionally, the focus was on ensuring fast response times and an attractive interface to highlight promotions.',
+        image: pinascoImg
     },
     {
         id: 3,
-        titleSpanish: 'Comunidad de Cafeteria.',
-        titleEnglish: 'Coffee Lovers Community.',
-        description: 'Diseño de una página web para una comunidad de amantes del café. El proyecto incluye un blog, una tienda en línea y una sección de eventos.',
-        image: ''
+        titleSpanish: 'Venta de Instrumentos.',
+        titleEnglish: 'Musical Instruments Store.',
+        descriptionSpanish: 'Diseño de una página web para una comunidad de amantes del café. El proyecto incluye un blog, una tienda en línea y una sección de eventos.',
+        descriptionEnglish: 'Design of a website for a community of coffee lovers. The project includes a blog, an online store, and an events section.',
+        desafioSpanish: 'El desafío fue desarrollar una plataforma que permita explorar los instrumentos de forma dinámica, con descripciones detalladas, reseñas y contenido multimedia. También se trabajó en crear un proceso de compra fluido que refleje la calidad y el compromiso de la tienda.',
+        desafioEnglish: 'The challenge was to develop a platform that allows users to explore instruments dynamically, with detailed descriptions, reviews, and multimedia content. Additionally, the focus was on creating a seamless shopping process that reflects the store\'s quality and commitment.',
+        image: pinascoImg
     }
-]
+];
