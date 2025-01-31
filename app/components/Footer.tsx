f00c2549c59fe439de8aedc4aa63902a098e59b9
Federico Herrera <fede.juan.herrera@gmail.com>
2025-01-31 15:27:37 -0300

    improving footer and sheet in mobile view

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -23,10 +23,10 @@ export const Footer = () => {
         <footer className={`h-[70vh] ${nightMode ? 'bg-black' : ''} pt-20`}>
             <div className={`${nightMode ? 'bg-zinc-950' : 'bg-zinc-100'} flex flex-col gap-5 items-center h-full w-full`}>
                 <div className="flex gap-3 items-center justify-center pt-20">
-                    <Image src={nightMode ? logoLight : logoDark} alt="" className="md:w-12 w-10 h-auto rotate-45" />
-                    <h1 className={`${nightMode ? 'text-zinc-300' : 'text-black'} md:text-5xl text-3xl`}>AsNeeed</h1>
+                    <Image src={nightMode ? logoLight : logoDark} alt="" className="md:w-10 w-8 h-auto rotate-45" />
+                    <h1 className={`${nightMode ? 'text-zinc-300' : 'text-black'} md:text-4xl text-2xl`}>AsNeeed</h1>
                 </div>
-                <p className={`${nightMode ? 'text-zinc-400' : 'text-zinc-600'} md:text-base text-sm text-center`}>{spanish ? 'Encontraste el mejor lugar para comenzar con tu negocio.' : 'You found the right place to start your own business'}</p>
+                <p className={`${nightMode ? 'text-zinc-400' : 'text-zinc-600'} md:text-lg text-base text-center`}>{spanish ? 'Transformamos tus ideas en realidad digital.' : 'We turn your ideas into digital reality.'}</p>
                 <div className={`flex items-center gap-3 md:text-base text-sm ${nightMode ? 'text-zinc-400' : 'text-zinc-600'} mt-5`}>
                     <FaRegCopyright />
                     <p>2025 AsNeeed - {spanish ? 'Todos los derechos reservados' : 'All rights reserved'}</p>
