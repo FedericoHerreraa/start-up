19bc9767bf33a3417804bf39238f399fb721f334
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-03 19:07:34 -0300

    correcting icon from footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -24,7 +24,7 @@ export const Footer = () => {
         <footer className={`h-[70vh] ${nightMode ? 'bg-black' : ''} pt-20`}>
             <div className={`${nightMode ? 'bg-zinc-950' : 'bg-zinc-100'} flex flex-col gap-5 items-center h-full w-full`}>
                 <div className="flex gap-1 items-center justify-center pt-20">
-                    <Image src={nightMode ? logoDark : logoLight} alt="" className="md:w-[85px] w-8 h-auto" />
+                    <Image src={nightMode ? logoDark : logoLight} alt="" className="md:w-[85px] w-16 h-auto" />
                     <h1 className={`${nightMode ? 'text-zinc-300' : 'text-black'} md:text-4xl text-2xl`}>AsNeeed</h1>
                 </div>
                 <p className={`${nightMode ? 'text-zinc-400' : 'text-zinc-600'} md:text-lg text-base text-center`}>{spanish ? 'Transformamos tus ideas en realidad digital.' : 'We turn your ideas into digital reality.'}</p>
