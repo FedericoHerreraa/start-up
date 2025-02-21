b0098203f2773db0810ab5b1a020975d5bbb9572
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-21 12:24:54 -0300

    changing landing

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -9,6 +9,7 @@ import imgLanding from "@/app/img/images/pngwing.com-4.png"
 import { useNightMode } from '@/app/context/NightModeContext'
 import { useLenguage } from '@/app/context/LenguageContext';
 import { useMobileView } from "@/app/context/MobileViewContext";
+import { FaHandshake } from 'react-icons/fa';
 
 
 export const LandingInfo = () => {
@@ -33,6 +34,12 @@ export const LandingInfo = () => {
                                 : 'At "AsNeeed", we turn your ideas into digital reality. We design and develop mobile applications, web platforms, and social media, fully customized to meet your business needs.'
                             }
                         </p>
+                        <div className="flex items-center gap-3">
+                            <FaHandshake size={25}/>
+                            <p className={`md:text-lg text-md md:text-left text-center ${nightMode ? 'text-zinc-200' : ''} `}>
+                                {spanish ? '' : 'Trusted by '}<span className="font-bold">10+</span> {spanish ? 'compañías confian en nosotros' : 'companies'}
+                            </p>
+                        </div>
                         <Link 
                             href='/start-now'
                             className={`bg-blue-800 text-white md:py-5 py-3 md:px-7 px-5 shadow-2xl rounded-xl w-fit hover:scale-110 hover:bg-blue-700 transform transition duration-300 ease-in-out`}
